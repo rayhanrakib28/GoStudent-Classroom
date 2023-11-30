fa9d356fa66481e9cbea04fa4bb73d59a3afee4c
developedbyrayhan <islamraihanofficial@gmail.com>
2023-12-01 02:33:08 +0600

    updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'https://gs-server.vercel.app',
+    baseURL: 'http://localhost:5000',
     withCredentials: true
 })
 const useAxiosSecure = () => {
